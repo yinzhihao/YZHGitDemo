69c1cd2b502b8ceb30d95532846b8061e6dc49dd
yinzhihao <853488983@qq.com>
2015-10-16 19:27:06 +0800

    使用CocoaPods更新了AFN第三方库

---
+++ GitDemo/GitDemo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  GitDemo
+//
+//  Created by qianfeng on 15/10/12.
+//  Copyright (c) 2015年 yzh. All rights reserved.
+//
+
+#import "ViewController.h"
+//#import <AFNetworking.h>
+#import "AFNetworking.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
