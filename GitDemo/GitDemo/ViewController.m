0007b8088fd1b118e7d4235df1ebe90ad3abbe9b
yinzhihao <853488983@qq.com>
2015-10-16 20:42:04 +0800

    修改了背景色

--- GitDemo/GitDemo/ViewController.m
+++ GitDemo/GitDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <AFNetworking.h>
 
 @interface ViewController ()
 
@@ -17,6 +18,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor redColor];
 }
 
 - (void)didReceiveMemoryWarning {
